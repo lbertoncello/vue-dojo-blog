6031b00d157e04d08484cd80958459097d6cf147
lbertoncello <lucas_b_andrade@hotmail.com>
2022-01-24 18:52:19 -0300

    feat: basics from the composition api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="home">
     Home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  /* COMPOSITION API */
+  setup() {
+    console.log('setup')
+
+    // Not reactive values
+    let name = 'Mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('You cliked me')
+    }
+
+    return {
+      name,
+      age,
+      handleClick
+    }
+  }
 }
 </script>
