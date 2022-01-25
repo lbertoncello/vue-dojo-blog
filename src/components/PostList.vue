8265a32b11a1a83a0672c921837703e405983ecd
lbertoncello <lucas_b_andrade@hotmail.com>
2022-01-25 19:02:57 -0300

    class: using props in the setup function

---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+      <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SinglePost from './SinglePost.vue'
+
+export default {
+  name: 'PostList',
+  props: [ 'posts' ],
+  components: { SinglePost },
+  // This is how we access props inside the setup function
+  setup(props) {
+    console.log(props.posts)
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,64 +1,25 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>search term - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
-    <button @click="handleClick">Stop watching</button>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-import { computed, ref, watch, watchEffect } from 'vue'
+import { ref } from 'vue'
+import PostList from '../components/PostList.vue'
 
 export default {
   name: 'Home',
+  components: { PostList },
   /* COMPOSITION API */
   setup() {
-    const search = ref('')
-    const names = ref([ 
-      'mario', 
-      'yoshi', 
-      'luigi', 
-      'toad', 
-      'bowser', 
-      'koopa', 
-      'peach' 
+    const posts = ref([
+      { title: 'welcome to the blog', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et dolorem amet tenetur autem aspernatur, aliquid saepe eligendi, reiciendis quia ducimus neque iste ea nobis, repellendus esse quisquam hic. Accusantium nobis totam tenetur inventore non optio a earum dolorum! Labore doloremque iusto sapiente illo esse porro in, alias est voluptatibus error iure nostrum mollitia magnam commodi vel quasi! Recusandae, qui consequuntur!', id: 1 },
+      { title: 'top 5 CSS tips', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et dolorem amet tenetur autem aspernatur, aliquid saepe eligendi, reiciendis quia ducimus neque iste ea nobis, repellendus esse quisquam hic. Accusantium nobis totam tenetur inventore non optio a earum dolorum! Labore doloremque iusto sapiente illo esse porro in, alias est voluptatibus error iure nostrum mollitia magnam commodi vel quasi! Recusandae, qui consequuntur!', id: 2 }
     ])
 
-    /*
-     * 'watch' binds a function which will run every time that 'search' has its value changed.
-     */
-    const stopWatch = watch(search, () => {
-      console.log('watch function ran')
-    })
-
-    /*
-     * Binds a function which will run every time that any of the vars 
-     * inside the function has its value changed.
-     * When the setup is loaded, the function automatically runs once.
-     */
-    const stopEffect = watchEffect(() => {
-      console.log('watchEffect function ran', search.value)
-    })
-
-    // Computed property
-    const matchingNames = computed(() => names.value.filter(
-        (name) => name.includes(search.value)
-      )
-    )
-
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
-
-    return {
-      names,
-      search,
-      matchingNames,
-      handleClick
-    }
+    return { posts }
   }
 }
 </script>
