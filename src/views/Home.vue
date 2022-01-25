4e94758adc1138de94aa806d12e5da1c145ffbe1
lbertoncello <lucas_b_andrade@hotmail.com>
2022-01-25 16:06:36 -0300

    class: using props in the setup function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,12 @@
     <input type="text" v-model="search">
     <p>search term - {{ search }}</p>
     <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">Stop watching</button>
   </div>
 </template>
 
 <script>
-import { computed, ref } from 'vue'
+import { computed, ref, watch, watchEffect } from 'vue'
 
 export default {
   name: 'Home',
@@ -25,15 +26,38 @@ export default {
       'peach' 
     ])
 
+    /*
+     * 'watch' binds a function which will run every time that 'search' has its value changed.
+     */
+    const stopWatch = watch(search, () => {
+      console.log('watch function ran')
+    })
+
+    /*
+     * Binds a function which will run every time that any of the vars 
+     * inside the function has its value changed.
+     * When the setup is loaded, the function automatically runs once.
+     */
+    const stopEffect = watchEffect(() => {
+      console.log('watchEffect function ran', search.value)
+    })
+
+    // Computed property
     const matchingNames = computed(() => names.value.filter(
         (name) => name.includes(search.value)
       )
     )
 
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
+
     return {
       names,
       search,
-      matchingNames
+      matchingNames,
+      handleClick
     }
   }
 }
