a0474d124322219e8d0ed72833a8a06c362b43bf
lbertoncello <lucas_b_andrade@hotmail.com>
2022-01-25 14:42:55 -0300

    class: template refs in the setup function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,34 @@
 <template>
   <div class="home">
     Home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'Home',
   /* COMPOSITION API */
   setup() {
-    console.log('setup')
+    const p = ref(null)
 
     // Not reactive values
     let name = 'Mario'
     let age = 30
 
     const handleClick = () => {
-      console.log('You cliked me')
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'hello, ninjas'
     }
 
     return {
       name,
       age,
+      p,
       handleClick
     }
   }
