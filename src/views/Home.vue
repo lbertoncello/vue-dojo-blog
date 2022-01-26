a294cff968762ab9ff751f65429fdc1f87257f38
lbertoncello <lucas_b_andrade@hotmail.com>
2022-01-26 17:52:17 -0300

    chore: add json-server to mock the api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts" />
-    <button @click="handleTogglePost">toggle post</button>
-    <button @click="handleDeletePost">delete a post</button>
+    <PostList :posts="posts" />
   </div>
 </template>
 
@@ -18,19 +16,10 @@ export default {
   setup() {
     const posts = ref([
       { title: 'welcome to the blog', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et dolorem amet tenetur autem aspernatur, aliquid saepe eligendi, reiciendis quia ducimus neque iste ea nobis, repellendus esse quisquam hic. Accusantium nobis totam tenetur inventore non optio a earum dolorum! Labore doloremque iusto sapiente illo esse porro in, alias est voluptatibus error iure nostrum mollitia magnam commodi vel quasi! Recusandae, qui consequuntur!', id: 1 },
-      { title: 'top 5 CSS tips', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et dolorem amet tenetur autem aspernatur, aliquid saepe eligendi, reiciendis quia ducimus neque iste ea nobis, repellendus esse quisquam hic. Accusantium nobis totam tenetur inventore non optio a earum dolorum! Labore doloremque iusto sapiente illo esse porro in, alias est voluptatibus error iure nostrum mollitia magnam commodi vel quasi! Recusandae, qui consequuntur!', id: 2 }
+      { title: 'top 5 CSS tips', body: 'Lorem ipsum', id: 2 }
     ])
-    const showPosts = ref(true)
-
-    const handleTogglePost = () => showPosts.value = !showPosts.value
-    const handleDeletePost = () => posts.value.pop()
     
-    return { 
-      posts, 
-      showPosts, 
-      handleTogglePost,
-      handleDeletePost
-    }
+    return { posts }
   }
 }
 </script>
