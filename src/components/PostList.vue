22699ef5cbff568f103093b41b7f074aa9007703
lbertoncello <lucas_b_andrade@hotmail.com>
2022-01-26 18:28:48 -0300

    refactor: remove unnecessary code

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -7,23 +7,12 @@
 </template>
 
 <script>
-import { onMounted, onUnmounted, onUpdated } from 'vue'
 import SinglePost from './SinglePost.vue'
 
 export default {
   name: 'PostList',
   props: [ 'posts' ],
   components: { SinglePost },
-  // This is how we access props inside the setup function
-  setup(props) {
-    // Lifecycle hooks
-    onMounted(() => console.log('component mounted'))
-    onUnmounted(() => console.log('component unmounted'))
-    onUpdated(() => console.log('component updated'))
-  },
-  mounted() {
-    console.log('mounted using options api')
-  }
 }
 </script>
 
