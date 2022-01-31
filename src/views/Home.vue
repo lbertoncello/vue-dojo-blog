5d267d520e34be8682a73aa535f29334e5a1da2c
lbertoncello <lucas_b_andrade@hotmail.com>
2022-01-31 19:34:38 -0300

    feat(Home): add component TagCloud

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,9 @@
   <div class="home">
     <h1>Home</h1>
     <div v-if="error">{{ error }}</div>
-    <div v-if="posts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="posts" />
+      <TagCloud :posts=posts />
     </div>
     <div v-else>
       <Spinner />
@@ -14,11 +15,12 @@
 <script>
 import PostList from '../components/PostList.vue'
 import Spinner from '../components/Spinner.vue'
+import TagCloud from '../components/TagCloud.vue'
 import getPosts from '../composables/getPosts'
 
 export default {
   name: 'Home',
-  components: { PostList, Spinner },
+  components: { PostList, Spinner, TagCloud },
   /* COMPOSITION API */
   setup() {
     const { posts, error, load } = getPosts()
@@ -35,5 +37,10 @@ export default {
     max-width: 1200px;
     margin: 0 auto;
     padding: 10px;
+  }
+  .layout {
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    gap: 20px
   }
 </style>
