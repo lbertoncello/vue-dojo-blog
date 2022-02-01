127253a3c9850ee5e11446b1968b191e013d0268
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-01 18:06:08 -0300

    feat: setup firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8OAG3JP7uNBAZKS-9Ern7iEp8aeARet4",
+  authDomain: "udemy-vue-firebase-sites-599e4.firebaseapp.com",
+  projectId: "udemy-vue-firebase-sites-599e4",
+  storageBucket: "udemy-vue-firebase-sites-599e4.appspot.com",
+  messagingSenderId: "1049648309488",
+  appId: "1:1049648309488:web:b7d11a77b4403b6f2dce35",
+  measurementId: "G-S2F4RMFVZH"
+};
+
+// Init Firebase
+firebase.initializeApp(firebaseConfig)
+
+// Init Firestore Service
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
