5e220f4e0adaaa4e111c2af93d8ba5707489f342
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-01 19:01:00 -0300

    refactor: change getPost to get data from Firebase

--- src/composables/getPost.js
+++ src/composables/getPost.js
@@ -1,4 +1,5 @@
 import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
 
 const getPost = (id) => {
   const uri = `http://localhost:3000/posts/${id}`
@@ -7,16 +8,8 @@ const getPost = (id) => {
 
   const load = async () => {
     try {
-      // Simulate delay
-      await new Promise((resolve) => {
-        setTimeout(resolve, 2000)
-      })
-
-      let data = await fetch(uri)
-      if (!data.ok) {
-        throw Error('That post does not exist')
-      }
-      post.value = await data.json()
+      const res = await projectFirestore.collection('posts').doc(id).get()
+      post.value = { id: res.id, ...res.data() }
     } catch(err) {
       error.value = err.message
       console.error(error.value)
