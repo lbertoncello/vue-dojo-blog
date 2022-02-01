8830b7308338ec501f837cbae940c4018fe2216b
lbertoncello <lucas_b_andrade@hotmail.com>
2022-02-01 19:09:24 -0300

    refactor: change createPost to save data in Firebase

--- src/composables/createPost.js
+++ src/composables/createPost.js
@@ -1,20 +1,12 @@
 import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
 
 const createPost = () => {
-  const uri = 'http://localhost:3000/posts'
   const error = ref(null)
 
   const create = async (post) => {
     try {
-      const data = await fetch(uri, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(post)
-      })
-
-      if (!data.ok) {
-        throw Error('Error trying to create a new post')
-      }
+      await projectFirestore.collection('posts').add(post)
     } catch (err) {
       error.value = err
       console.error(error.value)
